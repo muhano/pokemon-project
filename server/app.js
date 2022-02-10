68c104a87e37eefbfd570143a6fa4e5fbd2cf21f
muhano <geri.muhano@gmail.com>
2022-02-11 06:12:25 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const router = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json()) // for parsing application/json
